a8ca3ac68dbd3c2a9e246d21ec7f88fb23ef17b5
Anthony Howard <ahowar27@baker.edu>
2016-08-21 19:44:33 -0400

    Updated Model and Controllers

---
+++ MusicStore/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicStore.Models
+{
+    public class Artist
+    {
+
+        public int ArtistID { get; set; }
+
+        public String ArtistName { get; set; }
+
+        public virtual List<Album> Albums { get; set; }
+
+    }
+}
---
+++ MusicStore/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Models
+{
+    public class Review
+    {
+
+        public int ReviewID { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public string Contents { get; set; }
+
+        [Required()]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+
+    }
+}
